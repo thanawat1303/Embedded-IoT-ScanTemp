6c0aa175d4817a3cc24d33467c2230243760a899
thanawat1303 <fquarius13@gmail.com>
2023-09-15 01:44:09 +0700

    edit code

--- CodeProject/CodeProject.ino
+++ CodeProject/CodeProject.ino
@@ -1,13 +1,13 @@
 #include <Wire.h>
-#include <LiquidCrystal.h>
+// #include <LiquidCrystal.h>
 #include <DFRobot_RGBLCD1602.h>
 
 // LCD normal
-const int rs = 8 , enable = 9 , d1 = 1 , d2 = 2 , d3 = 3 , d4 = 4;
-LiquidCrystal lcd(rs , enable , d1 , d2 , d3 , d4);
+// const int rs = 8 , enable = 9 , d1 = 1 , d2 = 2 , d3 = 3 , d4 = 4;
+// LiquidCrystal lcd(rs , enable , d1 , d2 , d3 , d4);
 
 // LCD I2C
-// DFRobot_RGBLCD1602 lcdI2C(16 , 2);
+DFRobot_RGBLCD1602 lcdI2C(16 , 2);
 
 #define TemPin A0
 #define LED_RED 7
@@ -18,14 +18,21 @@ LiquidCrystal lcd(rs , enable , d1 , d2 , d3 , d4);
 #define ButtonDOWN 11
 #define ChangeSetting 12
 
-bool state_start;
+const int DisplayAll = 0;
+const int DisplayTemHigh = 1;
+const int DisplayTemLow = 2; 
+
+const int arrLED[3] = { LED_RED , LED_GREEN , LED_BLUE };
+bool ProgramStart;
 
 int temHIGH;
 int temLOW;
-int state_change;
+int stateProgram;
 
 void setup() {
-  lcd.begin(16, 2);
+  // lcdI2C.begin(16, 2);
+  lcdI2C.init();
+  lcdI2C.setBacklight(true);
 
   pinMode(ButtonUP, INPUT);
   pinMode(ButtonDOWN, INPUT);
@@ -35,72 +42,73 @@ void setup() {
   pinMode(LED_GREEN, OUTPUT);
   pinMode(LED_RED, OUTPUT);
 
-  state_start = true;
-  state_change = 0;
-  temHIGH = 35;
+  ProgramStart = true;
+  stateProgram = 0;
+  temHIGH = 36;
   temLOW = 18;
 }
 
 void loop() {
   // ห้ามต่อ output ยาว ค่าจะเพี้ยน LM35
   if (digitalRead(ChangeSetting) == 1) {
-    state_change = state_change + 1;
-    if (state_change > 2) state_change = 0;
+    stateProgram = stateProgram + 1;
+    if (stateProgram > 2) stateProgram = 0;
 
-    lcd.clear();
-    lcd.print("Change menu");
+    lcdI2C.clear();
+    lcdI2C.print("Change menu");
     while (digitalRead(ChangeSetting) == 1);
-    lcd.clear();
+    lcdI2C.clear();
 
-  } else if (digitalRead(ButtonUP) == 1 && (state_change == 1 || state_change == 2)) {
-    switch (state_change) {
+  } else if (digitalRead(ButtonUP) == 1 && (stateProgram == 1 || stateProgram == 2)) {
+    switch (stateProgram) {
       case 1:
-        temHIGH = temHIGH + 1;
+          temHIGH = temHIGH + 1;
         break;
       case 2:
-        if(temLOW + 1 < temHIGH) temLOW = temLOW + 1;
+          if(temLOW + 1 < temHIGH) temLOW = temLOW + 1;
         break;
     }
     DisplayLCD("", "UP");
     while (digitalRead(ButtonUP) == 1);
-    lcd.clear();
-  } else if (digitalRead(ButtonDOWN) == 1 && (state_change == 1 || state_change == 2)) {
-    switch (state_change) {
+    lcdI2C.clear();
+  } else if (digitalRead(ButtonDOWN) == 1 && (stateProgram == 1 || stateProgram == 2)) {
+    switch (stateProgram) {
       case 1:
-        if(temHIGH - 1 > temLOW) temHIGH = temHIGH - 1;
+          if(temHIGH - 1 > temLOW) temHIGH = temHIGH - 1;
         break;
       case 2:
-        temLOW = temLOW - 1;
+          temLOW = temLOW - 1;
         break;
     }
     DisplayLCD("", "DOWN");
     while (digitalRead(ButtonDOWN) == 1);
-    lcd.clear();
+    lcdI2C.clear();
   }
 
-  switch (state_change) {
-    case 0:
-      DisplayLCD("H : " + String(temHIGH) + " : " + "C : " + String(temLOW) , "Tem : " + String(analogRead(TemPin) * 0.48828125));
+  switch (stateProgram) {
+    case DisplayAll:
+        DisplayLCD("H : " + String(temHIGH) + " : " + "C : " + String(temLOW) , "Tem : " + String(analogRead(TemPin) * 0.48828125));
       break;
-    case 1:
-      DisplayLCD("HOT : " + String(temHIGH), "");
+    case DisplayTemHigh:
+        DisplayLCD("HOT : " + String(temHIGH), "");
       break;
-    case 2:
-      DisplayLCD("COOL : " + String(temLOW), "");
+    case DisplayTemLow:
+        DisplayLCD("COOL : " + String(temLOW), "");
       break;
   }
 
-  DisplayLED_temperature();
+  DisplayLedTemperature();
 }
 
 
 
-void DisplayLED_temperature() {
+void DisplayLedTemperature() {
   float temperature = analogRead(TemPin) * 0.48828125;  // ((analog / 1024) * 5000) / 10
-  if (state_start) {
+  if (ProgramStart) {
     delay(100);
-    state_start = false;
+    ProgramStart = false;
   }
+
   if (temperature >= temHIGH) SelectLED(LED_RED);
   else if (temperature > temLOW && temperature < temHIGH) SelectLED(LED_GREEN);
   else if (temperature <= temLOW) SelectLED(LED_BLUE);
@@ -112,16 +120,12 @@ void DisplayLED_temperature() {
 }
 
 void DisplayLCD(String Row1 , String Row2) {
-  lcd.setCursor(0, 0);
-  lcd.print(Row1);
-  lcd.setCursor(0, 1);
-  lcd.print(Row2);
+  lcdI2C.setCursor(0, 0);
+  lcdI2C.print(Row1);
+  lcdI2C.setCursor(0, 1);
+  lcdI2C.print(Row2);
 }
 
 void SelectLED(int select) {
-  const int arrLED[3] = { LED_RED, LED_GREEN, LED_BLUE };
-  for (int x; x < 3; x++) {
-    if (arrLED[x] == select) digitalWrite(arrLED[x], HIGH);
-    else digitalWrite(arrLED[x], LOW);
-  }
+  for (int x; x < 3; x++) digitalWrite(arrLED[x], (arrLED[x] == select) ? HIGH : LOW);
 }
