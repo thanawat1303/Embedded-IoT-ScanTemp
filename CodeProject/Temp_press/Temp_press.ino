c37744f0b4e078c2dea56d3624338deba8452d73
thanawat1303 <fquarius13@gmail.com>
2023-10-17 00:21:34 +0700

    update pcb

--- CodeProject/Temp_press/Temp_press.ino
+++ CodeProject/Temp_press/Temp_press.ino
@@ -6,12 +6,12 @@
 // LiquidCrystal lcd(rs , enable , d1 , d2 , d3 , d4);
 
 // LCD I2C on Proteus
-#include <DFRobot_RGBLCD1602.h>
-DFRobot_RGBLCD1602 lcdI2C(16, 2);
+// #include <DFRobot_RGBLCD1602.h>
+// DFRobot_RGBLCD1602 lcdI2C(16, 2);
 
 // LCD I2C on Board
-// #include <LiquidCrystal_I2C.h>
-// LiquidCrystal_I2C lcdI2C(0x27, 16, 2);
+#include <LiquidCrystal_I2C.h>
+LiquidCrystal_I2C lcdI2C(0x27, 16, 2);
 
 // DS18B20
 #include <OneWire.h>
@@ -178,7 +178,7 @@ float GetTemp() {
 void DisplayLCDTemperature() {
   switch (stateProgram) {
     case DisplayAll:
-      DisplayLCD("H : " + String(temHIGH) + " : " + "C : " + String(temLOW), "Tem : " + String(TemperatureGet));
+      DisplayLCD("H : " + String(temHIGH) + " : " + "C : " + String(temLOW), "Temp : " + String(TemperatureGet));
       break;
     case DisplayTemHigh:
       DisplayLCD("OFFSET", "  HOT : " + String(temHIGH));
